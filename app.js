7b72aca887ab93b4b8ebf0af577bcacbe2698583
Dennis Haegler <dennis.haegler@gmail.com>
2022-01-06 16:29:07 +0100

    Try to use vue
    it uses vue2 because vuetify dont work

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Hello World')
+new Vue({
+  el: '#app',
+  vuetify: new Vuetify(),
+  data() {
+    return {
+      title: 'This is a track picker for ACC GT Series',
+      tracks: ['Barcelona', 'Silverstone', 'Nürnburgring']
+    }
+  }
+})

464943f5ed3ffc55b73e6298537255940121a757
Dennis Haegler <dennis.haegler@gmail.com>
2022-01-14 02:05:00 +0100

    Upgrade to vue3 and remove vuetify 2

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-new Vue({
+const racetracks = [
+  {
+    title: 'Monza',
+    length: '5.7',
+    img: 'https://cdn.discordapp.com/attachments/826345100031819787/929131992505802872/Monza.JPG',
+  },
+  {
+    title: 'Misano',
+    length: '4.2',
+    img: 'https://cdn.discordapp.com/attachments/826345100031819787/929131992275095623/Misano.JPG'
+  },
+  {
+    title: 'Kyalami',
+    length: '4.5',
+    img: 'https://cdn.discordapp.com/attachments/826345100031819787/929131992023445544/Kyalami.JPG'
+  },
+]
+const app = Vue.createApp({
   el: '#app',
-  vuetify: new Vuetify(),
-  data: {
-    title: 'This is a track picker for ACC GT Series',
-      tracks: [
-        {
-          title: 'Monza',
-          length: '5.7',
-          img: 'https://cdn.discordapp.com/attachments/826345100031819787/929131992505802872/Monza.JPG',
-        },
-        {
-          title: 'Misano',
-          length: '4.2',
-          img: 'https://cdn.discordapp.com/attachments/826345100031819787/929131992275095623/Misano.JPG'
-        },
-        {
-          title: 'Kyalami',
-          length: '4.5',
-          img: 'https://cdn.discordapp.com/attachments/826345100031819787/929131992023445544/Kyalami.JPG'
-        },
-      ]
+  data() {
+    return {
+      title: 'This is a track picker for ACC GT Series',
+      tracks: racetracks,
+    }
   },
   methods: {
     deleteRandomMap: function() {
@@ -28,3 +30,5 @@ new Vue({
     }
   }
 })
+
+app.mount('#app')

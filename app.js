6417acd67f14cf1c75882e3bb2ce7ff61a44eea3
Dennis Haegler <dennis.haegler@gmail.com>
2022-01-17 17:15:09 +0100

    Add more randomization

--- app.js
+++ app.js
@@ -17,21 +17,44 @@ const racetracks = [
 ]
 const app = Vue.createApp({
   el: '#app',
+  beforeMount() {
+    this.setTrackOnRandomSpots()
+  },
   components: {
     "p-card": primevue.card,
     "p-button": primevue.button,
-    'p-image': primevue.image
+    'p-image': primevue.image,
+    "p-inputnumber": primevue.inputnumber
   },
   data() {
     return {
       title: 'This is a track picker for ACC GT Series',
-      tracks: racetracks,
+      randomRunner: 5,
+      started: false,
+      tracks: [],
     }
   },
   methods: {
     deleteRandomMap: function() {
       const indexToDelete = Math.floor(Math.random() * this.tracks.length);
       this.tracks.splice(indexToDelete, 1)
+    },
+    setTrackOnRandomSpots: function() {
+      const len = racetracks.length
+      let copyOfTracks = [...racetracks]
+      this.tracks = []
+      for (let i = 0; i < len; i++) {
+        console.log('for real')
+        const indexToDelete = Math.floor(Math.random() * (len - i));
+        let track = copyOfTracks.splice(indexToDelete, 1)
+        this.tracks.push(track[0])
+      }
+    },
+    repeatRandomSetup: function() {
+      for (let i = 0; i < this.randomRunner; i++) {
+        this.setTrackOnRandomSpots()
+      }
+      this.started = true;
     }
   }
 })
